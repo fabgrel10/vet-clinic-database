ba80e2a8108fe0f855134d51b22bb9d6393b62aa
Fabricio Garcia <fabgrel10@gmail.com>
2022-03-21 15:35:31 -0600

    🩹 fix: Correct some data and queries

--- data.sql
+++ data.sql
@@ -1,10 +1,7 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals VALUES(1, 'Agumon', '2020-02-03', 0, true, 10.23);
-INSERT INTO animals VALUES(2, 'Gabumon', '2018-11-18', 2, true, 8);
+INSERT INTO animals VALUES(2, 'Gabumon', '2018-11-15', 2, true, 8);
 INSERT INTO animals VALUES(3, 'Pikachu', '2021-01-07', 1, false, 15.04);
 INSERT INTO animals VALUES(4, 'Devimon', '2017-05-12', 5, true, 11);
-SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
-SELECT * FROM animals WHERE neutered = true;
-SELECT * FROM animals WHERE name != 'Gabumon';
-SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
--- queries.sql
+++ queries.sql
@@ -2,7 +2,9 @@
 
 SELECT * FROM animals WHERE name like '%mon';
 SELECT * FROM animals WHERE date_of_birth >= '2016-01-01' AND date_of_birth <= '2019-01-01';
-SELECT * FROM animals WHERE escape_attempts > 3;
 SELECT * FROM animals WHERE neutered = true AND escape_attempts < 3;
-SELECT * FROM animals WHERE weight_kg > 10;
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' or name = 'Pikachu';
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered = true;
+SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
