760baf7fbfe1dbe3939a18a71eced8a178025bf2
Fabricio Garcia <fabgrel10@gmail.com>
2022-03-21 15:39:25 -0600

    🩹 fix: Correct some data and queries

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name like '%mon';
-SELECT * FROM animals WHERE date_of_birth >= '2016-01-01' AND date_of_birth <= '2019-01-01';
+SELECT * FROM animals WHERE date_of_birth >= '2016-01-01' AND date_of_birth <= '2019-31-12';
 SELECT * FROM animals WHERE neutered = true AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' or name = 'Pikachu';
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
