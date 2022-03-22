1bebba2629ffcc0f95df69ba22035e8f65bb1dfc
Fabricio Garcia <fabgrel10@gmail.com>
2022-03-22 12:46:08 -0600

    ✨ feat: Add a column species to table animals

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals(
-id int,
-name varchar(50),
-date_of_birth date,
-escape_attempts int,
-neutered boolean,
-weight_kg decimal
+ CREATE TABLE animals (
+	id INT PRIMARY KEY,
+	name VARCHAR (50) NOT NULL,
+	date_of_birth DATE NOT NULL,
+	escape_attempts VARCHAR ( 255 ) NOT NULL,
+  neutered BOOLEAN NOT NULL,
+	weight_kg DECIMAL NOT NULL,
+  species VARCHAR (50) NOT NULL
 );
