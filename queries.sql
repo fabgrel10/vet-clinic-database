0d62e15e72fb2688e6ae577175c5eab9baa73ac7
Fabricio Garcia <fabgrel10@gmail.com>
2022-03-23 12:54:24 -0600

    ✨ feat: Add milestone 3 alterations and queries

--- queries.sql
+++ queries.sql
@@ -1,6 +1,7 @@
 /* Queries that provide answers to the questions from all projects. */
 
 /* MILESTONE 1: create animals table*/
+
 SELECT * FROM animals WHERE name like '%mon';
 SELECT * FROM animals WHERE date_of_birth >= '2016-01-01' AND date_of_birth <= '2019-31-12';
 SELECT * FROM animals WHERE neutered = true AND escape_attempts < 3;
@@ -11,6 +12,7 @@ SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
 /* MILESTONE 2: query and update animals table */
+
 BEGIN;
 UPDATE animals SET species = 'unspecified';
 SELECT * FROM animals;
@@ -36,3 +38,14 @@ SELECT AVG(weight_kg) FROM animals;
 SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
 SELECT species, MAX(weight_kg), MIN(weight_kg) FROM animals GROUP BY species;
 SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31';
+
+/* MILESTONE 3: query multiple tables */
+
+SELECT name, full_name FROM owners INNER JOIN animals ON owners.id = animals.owner_id WHERE owners.full_name = 'Melody Pond';
+SELECT species.name, animals.name FROM species INNER JOIN animals ON species.id = animals.species_id WHERE species.name = 'Pokemon';
+SELECT name, full_name FROM owners LEFT JOIN animals ON owners.id = animals.owner_id;
+SELECT COUNT(animals.name) FROM species INNER JOIN animals ON species.id = animals.species_id WHERE species.name = 'Pokemon';
+SELECT COUNT(animals.name) FROM species INNER JOIN animals ON species.id = animals.species_id WHERE species.name = 'Digimon';
+SELECT name FROM animals INNER JOIN owners ON owners.id = animals.owner_id WHERE owners.full_name = 'Jennifer Orwell' AND animals.species_id = 2;
+SELECT animals.name, full_name FROM owners INNER JOIN animals ON owners.id = animals.owner_id WHERE full_name = 'Dean Winchester' AND animals.escape_attempts = 0;
+SELECT COUNT(animals.name), owners.full_name FROM owners INNER JOIN animals ON owners.id = animals.owner_id GROUP BY owners.full_name;
