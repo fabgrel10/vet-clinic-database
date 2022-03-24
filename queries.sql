5a3f149acd02e6c54df33aaa8298b83febc46205
Fabricio Garcia <fabgrel10@gmail.com>
2022-03-24 15:57:55 -0600

    💄 style: Format data from all files for better human readability

--- queries.sql
+++ queries.sql
@@ -1,57 +1,145 @@
 /* Queries that provide answers to the questions from all projects. */
 
-/* MILESTONE 1: create animals table*/
+/* MILESTONE 1*/
+SELECT *
+FROM animals
+WHERE name like '%mon';
 
-SELECT * FROM animals WHERE name like '%mon';
-SELECT * FROM animals WHERE date_of_birth >= '2016-01-01' AND date_of_birth <= '2019-31-12';
-SELECT * FROM animals WHERE neutered = true AND escape_attempts < 3;
-SELECT date_of_birth FROM animals WHERE name = 'Agumon' or name = 'Pikachu';
-SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
-SELECT * FROM animals WHERE neutered = true;
-SELECT * FROM animals WHERE name != 'Gabumon';
-SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+SELECT *
+FROM animals
+WHERE date_of_birth >= '2016-01-01'
+  AND date_of_birth <= '2019-31-12';
 
-/* MILESTONE 2: query and update animals table */
+SELECT *
+FROM animals
+WHERE neutered = true
+  AND escape_attempts < 3;
+
+SELECT date_of_birth
+FROM animals
+WHERE name = 'Agumon'
+  or name = 'Pikachu';
+
+SELECT name,
+  escape_attempts
+FROM animals
+WHERE weight_kg > 10.5;
+
+SELECT *
+FROM animals
+WHERE neutered = true;
+
+SELECT *
+FROM animals
+WHERE name != 'Gabumon';
+
+SELECT *
+FROM animals
+WHERE weight_kg >= 10.4
+  AND weight_kg <= 17.3;
+
+/* MILESTONE 2 */
 
 BEGIN;
-UPDATE animals SET species = 'unspecified';
-SELECT * FROM animals;
+UPDATE animals
+SET species = 'unspecified';
+SELECT *
+FROM animals;
 ROLLBACK;
 
 BEGIN;
-UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon%';
-UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon%';
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
 COMMIT;
-SELECT * FROM animals;
+SELECT *
+FROM animals;
 
 BEGIN;
-DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
 SAVEPOINT animals_2022_deleted;
-UPDATE animals SET weight_kg = weight_kg * -1;
+UPDATE animals
+SET weight_kg = weight_kg * -1;
 ROLLBACK TO animals_2022_deleted;
-UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
 COMMIT;
 
-SELECT COUNT(name) from animals;
-SELECT COUNT(escape_attempts) FROM animals WHERE escape_attempts = 0;
-SELECT AVG(weight_kg) FROM animals;
-SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
-SELECT species, MAX(weight_kg), MIN(weight_kg) FROM animals GROUP BY species;
-SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31';
+SELECT COUNT(name)
+from animals;
 
-/* MILESTONE 3: query multiple tables */
-
-SELECT name, full_name FROM owners INNER JOIN animals ON owners.id = animals.owner_id WHERE owners.full_name = 'Melody Pond';
-SELECT species.name, animals.name FROM species INNER JOIN animals ON species.id = animals.species_id WHERE species.name = 'Pokemon';
-SELECT name, full_name FROM owners LEFT JOIN animals ON owners.id = animals.owner_id;
-SELECT COUNT(animals.name) FROM species INNER JOIN animals ON species.id = animals.species_id WHERE species.name = 'Pokemon';
-SELECT COUNT(animals.name) FROM species INNER JOIN animals ON species.id = animals.species_id WHERE species.name = 'Digimon';
-SELECT name FROM animals INNER JOIN owners ON owners.id = animals.owner_id WHERE owners.full_name = 'Jennifer Orwell' AND animals.species_id = 2;
-SELECT animals.name, full_name FROM owners INNER JOIN animals ON owners.id = animals.owner_id WHERE full_name = 'Dean Winchester' AND animals.escape_attempts = 0;
-SELECT owners.full_name AS "Owner",
-COUNT(animals.id) AS "Total Animals"
+SELECT COUNT(escape_attempts)
 FROM animals
-INNER JOIN owners
-ON animals.owner_id = owners.id
+WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg)
+FROM animals;
+
+SELECT neutered,
+  SUM(escape_attempts)
+FROM animals
+GROUP BY neutered;
+
+SELECT species,
+  MAX(weight_kg),
+  MIN(weight_kg)
+FROM animals
+GROUP BY species;
+
+SELECT AVG(escape_attempts)
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31';
+
+/* MILESTONE 3 */
+
+SELECT name,
+  full_name
+FROM owners
+  INNER JOIN animals ON owners.id = animals.owner_id
+WHERE owners.full_name = 'Melody Pond';
+
+SELECT species.name,
+  animals.name
+FROM species
+  INNER JOIN animals ON species.id = animals.species_id
+WHERE species.name = 'Pokemon';
+
+SELECT name,
+  full_name
+FROM owners
+  LEFT JOIN animals ON owners.id = animals.owner_id;
+SELECT COUNT(animals.name)
+FROM species
+  INNER JOIN animals ON species.id = animals.species_id
+WHERE species.name = 'Pokemon';
+
+SELECT COUNT(animals.name)
+FROM species
+  INNER JOIN animals ON species.id = animals.species_id
+WHERE species.name = 'Digimon';
+
+SELECT name
+FROM animals
+  INNER JOIN owners ON owners.id = animals.owner_id
+WHERE owners.full_name = 'Jennifer Orwell'
+  AND animals.species_id = 2;
+
+SELECT animals.name,
+  full_name
+FROM owners
+  INNER JOIN animals ON owners.id = animals.owner_id
+WHERE full_name = 'Dean Winchester'
+  AND animals.escape_attempts = 0;
+
+SELECT owners.full_name AS "Owner",
+  COUNT(animals.id) AS "Total Animals"
+FROM animals
+  INNER JOIN owners ON animals.owner_id = owners.id
 GROUP BY owners.full_name
-ORDER BY COUNT(animals.id) DESC LIMIT 1;
+ORDER BY COUNT(animals.id) DESC
+LIMIT 1;
--- schema.sql
+++ schema.sql
@@ -1,33 +1,41 @@
 /* Database schema to keep the structure of entire database. */
 
-/* MILESTONE 1: create animals table */
+/* MILESTONE 1 */
 
- CREATE TABLE animals (
-	id INT PRIMARY KEY,
-	name VARCHAR (50) NOT NULL,
-	date_of_birth DATE NOT NULL,
-	escape_attempts VARCHAR ( 255 ) NOT NULL,
-  neutered BOOLEAN NOT NULL,
-	weight_kg DECIMAL NOT NULL,
-  species VARCHAR (50) NOT NULL
+CREATE TABLE animals (
+	id SMALLINT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	name VARCHAR(20),
+	date_of_birth DATE,
+	escape_attempts SMALLINT,
+	neutered BOOLEAN,
+	weight_kg DECIMAL
 );
 
-/* MILESTONE 3: query multiple tables */
+/* MILESTONE 2 */
+
+ALTER TABLE animals
+ADD COLUMN species VARCHAR(50);
+
+/* MILESTONE 3 */
 
 CREATE TABLE owners(
-id  INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-full_name VARCHAR(100) NOT NULL,
-age SMALLINT NOT NULL
+	id SMALLINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	full_name VARCHAR(50),
+	age SMALLINT
 );
 
 CREATE TABLE species(
-id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,,
-name varchar(50) NOT NULL
+	id SMALLINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	name varchar(50)
 );
 
-ALTER TABLE animals DROP COLUMN id;
-ALTER TABLE animals ADD id SMALLINT NOT NULL GENERATED ALWAYS AS IDENTITY;
-ALTER TABLE animals DROP COLUMN species;
-ALTER TABLE animals ADD species_id INT REFERENCES species(id);
-ALTER TABLE animals ADD owner_id INT REFERENCES owners(id);
+ALTER TABLE animals
+DROP COLUMN species;
 
+ALTER TABLE animals
+ADD COLUMN species_id SMALLINT,
+ADD CONSTRAINT fk_animals_species FOREIGN KEY(species_id) REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id SMALLINT,
+ADD CONSTRAINT fk_animals_owners FOREIGN KEY(owner_id) REFERENCES owners(id);
